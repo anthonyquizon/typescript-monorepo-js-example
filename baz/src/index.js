eae95d82b41419cc6efd0ece2d963901477faf9e
Anthony Quizon <accounts@anthonyquizon.io>
2023-11-06 12:00:11 +1100

    baz package

--- baz/src/index.js
+++ baz/src/index.js
@@ -1,11 +1,16 @@
+
 import bar from "@packages/bar";
+import foo from "@packages/foo";
 /**
  * @param {number} n
  * @returns {number}
  */
-export function foo(n) {
+export function baz(n) {
     bar.bar(1)
+    foo.foo(1)
     return n+1;
 }
+
+
 
 
